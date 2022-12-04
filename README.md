f945d65ee56d25dada58e65a420bd8ab52b78001
Josue Cano <73917299+Omiced@users.noreply.github.com>
2022-12-04 16:59:21 +0000

    Agregada funcionalidad de borrar agregado icono de trash can

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ Modifique estilos para que todo se vea bien
 Tocó agregarle un form para añadir más regalos
 Agregue un scrip js, dentro hice la funcionalidad para añadir regalos
 modifique algunos estilos para que se vea decente.
+
+## Día 4
+
+Tocó poder eliminar regalos de la lista.
+Cambie código para tener un estado local, y poder manejar los regalos que se eliminan y los que se muestran.
+Hice uso de event delegation para escuchar los eventos en los elementos generados dinámicamente y poder borrar.
+Por el momento todo ok.
--- js/app.js
+++ js/app.js
@@ -1,17 +1,61 @@
 const addForm = document.querySelector(".regalos-form");
-const giftList = document.querySelector(".regalos-list");
+const giftListEl = document.querySelector(".regalos-list");
 const giftValue = document.querySelector("#regalo");
+const giftsContainerEl = document.querySelector(".regalos-list");
 
-const addGift = function (gift) {
-  const giftMarkup = `
-  <li class="regalos-item">${gift}</li>
-`;
-  giftList.insertAdjacentHTML("beforeEnd", giftMarkup);
+const giftsList = [];
+
+const renderGift = function () {
+  giftsList.forEach((gift, i) => {
+    if (gift.render) return;
+    gift.render = true;
+    giftListEl.insertAdjacentHTML(
+      "beforeEnd",
+      `
+    <li class="regalos-item">
+      ${gift.name}
+      <button type="button" class="delete-btn" data-item="${i}">
+        <img
+          src="images/icons/trash_icon.svg"
+          alt="trash can icon"
+          class="icon"
+          id="delete"
+        />
+      </button>
+    </li>
+  `
+    );
+  });
+};
+
+const pushGift = function (gift) {
+  giftsList.push({
+    name: gift,
+  });
+};
+
+const removeGift = function (gift) {
+  giftsList.splice(gift, 1);
+  updateList();
+};
+
+const updateList = function () {
+  giftsList.forEach((gift) => delete gift.render);
+  giftListEl.innerHTML = "";
+  renderGift();
 };
 
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const gift = giftValue.value;
   if (!giftValue.value) return;
-  addGift(gift);
+  pushGift(giftValue.value);
+  giftValue.value = "";
+  renderGift();
+});
+
+giftListEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.id === "delete") {
+    removeGift(e.target.dataset.item);
+  }
 });
